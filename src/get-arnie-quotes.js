4d3bc320a0919aecb1052b581bf172b823ffa547
John Owen <john.owen@aginic.com>
2021-02-09 14:06:52 +1000

    implement getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,15 @@
 const { httpGet } = require('./mock-http-interface');
 
-const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+const getArnieQuotes = (urls) => Promise.all(urls.map(getArnieQuote));
+
+const getArnieQuote = async (url) => {
+  const response = await httpGet(url);
+  const message = JSON.parse(response.body).message;
+  if (response.status === 200) {
+    return { 'Arnie Quote': message };
+  } else {
+    return { FAILURE: message };
+  }
 };
 
 module.exports = {
